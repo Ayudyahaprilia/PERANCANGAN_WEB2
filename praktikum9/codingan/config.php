bb294be30554e14d99d505a160eccd983757cdac
Ayudyahaprilia <dyahajaayu@gmail.com>
2025-12-16 10:33:53 +0700

    “Menambahkan file praktikum”  “Upload tugas praktikum”  “Initial commit”

---
+++ praktikum9/codingan/config.php
@@ -0,0 +1,11 @@
+<?php
+return [
+  'smtp_host' => 'smtp.gmail.com',
+  'smtp_port' => 587,
+  'smtp_user' => 'dyahajaayu@gmail.com',
+  'smtp_pass' => 'vlrnzbhimedabmgf',
+  'from_email' => 'dyahajaayu@gmail.com',
+  'from_name'  => 'Sistem Data Mahasiswa',
+  'to_email'   => 'dyahaprilia074@gmail.com',
+  'to_name'    => 'Penerima'
+];
